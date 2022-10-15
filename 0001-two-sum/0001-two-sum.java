84aec9f36a07517da00a16ca01c82c1294f9caa4
ChoeSul <jjgod0124@gmail.com>
2022-10-15 18:09:17 +0900

    Time: 14 ms (49.82%), Space: 46.8 MB (9.17%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -11,6 +11,8 @@ class Solution {
 
         for (int i = 0; i < nums.length; i++) {
             int num = nums[i];
+            //ex) 5(target)-3(index)=2(map.get) , 2+3=5
+            //map.get 이 NULL 이 아니다 말은 index 가 해가 된다는 뜻
             Integer k = map.get(target - num);
             if (k != null && k != i) {
                 return new int[]{i, k};
