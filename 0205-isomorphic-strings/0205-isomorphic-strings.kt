a2877bd2b2591f29b47e3ea61b4640c984e1ec95
ChoeSul <jjgod0124@gmail.com>
2022-11-24 21:56:25 +0900

    Time: 325 ms (64.96%), Space: 38.9 MB (41.80%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.kt
+++ 0205-isomorphic-strings/0205-isomorphic-strings.kt
@@ -1,12 +1,12 @@
 class Solution {
-   fun isIsomorphic(s: String, t: String): Boolean {
+    fun isIsomorphic(s: String, t: String): Boolean {
     val map = HashMap<Char, Char>()
     val set = HashSet<Char>()
     for (i in s.indices) {
         val sc = s[i]
         val tc = t[i]
-        if (map.containsKey(sc)) {
-            if (map[sc] != tc) {
+        if (map.containsKey(sc)) { // p
+            if (map[sc] != tc) { // p 를 t 라고 매핑 했는데 아니면 false!!
                 return false
             }
         } else {
@@ -14,14 +14,11 @@ class Solution {
             if (set.contains(tc)) {
                 return false
             }
-            map[sc] = tc
-            set.add(tc)
-        }
-        if (i == 0) {
-            map[s[i]] = t[i]
+            map[sc] = tc // p = t, a = i
+            set.add(tc) // t, i
         }
     }
 
     return true
-}
+    }
 }
